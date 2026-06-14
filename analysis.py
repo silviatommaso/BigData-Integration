5fecb32d5b4f023ecc35c8877cf31f80ec0d648f
silviatommaso <silvia.tommaso18112002@gmail.com>
2026-06-14 12:02:05 +0200

    small fixes

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import missingno as msno
 
-df_imdb = pd.read_csv("imdb.csv", low_memory=False)
+df_imdb = pd.read_csv("movies3/csv_files/rotten_tomatoes.csv", low_memory=False)
 df_rottentomatoes = pd.read_csv("rotten_tomatoes.csv", low_memory=False)
 df_rogerebert = pd.read_csv("roger_ebert.csv", low_memory=False)
 
@@ -10,23 +10,23 @@ df_rogerebert = pd.read_csv("roger_ebert.csv", low_memory=False)
 ##### DATA ANALYSIS #####
 #########################
 
-print("IMDB DATASET ANALYSIS\n")
-print("\n")
+# print("IMDB DATASET ANALYSIS\n")
+# print("\n")
 
-print("\n")
+# print("\n")
 
-print("NULL VALUES:\n")
-print(df_imdb.info())
-print("\n")
-null_values_imdb = df_imdb.isnull().sum()
-print(null_values_imdb)
+# print("NULL VALUES:\n")
+# print(df_imdb.info())
+# print("\n")
+# null_values_imdb = df_imdb.isnull().sum()
+# print(null_values_imdb)
 
-print("\n")
+# print("\n")
 
-print("NULL STATISTICS:\n ")
-null_imdb_values = df_imdb[["Creator", "Cast", "Duration", "RatingValue", "Genre", "Description"]].isnull().sum()
-percentage_null_imdb_values = null_imdb_values/len(df_imdb[["Creator", "Cast", "Duration", "RatingValue", "Genre", "Description"]])
-print(percentage_null_imdb_values)
+# print("NULL STATISTICS:\n ")
+# null_imdb_values = df_imdb[["Creator", "Cast", "Duration", "RatingValue", "Genre", "Description"]].isnull().sum()
+# percentage_null_imdb_values = null_imdb_values/len(df_imdb[["Creator", "Cast", "Duration", "RatingValue", "Genre", "Description"]])
+# print(percentage_null_imdb_values)
 
 ################################################################################################################################################
 
